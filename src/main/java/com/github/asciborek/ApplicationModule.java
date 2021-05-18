fe30486579ad1c3ded8511c629ce7af46fef9a95
Aleksander Ściborek <aleksandersciborek@gmail.com>
2021-05-19 00:08:32 +0200

    Replace annotated factory methods with binding in the configure method

--- src/main/java/com/github/asciborek/ApplicationModule.java
+++ src/main/java/com/github/asciborek/ApplicationModule.java
@@ -4,9 +4,7 @@ import com.github.asciborek.settings.SettingsService;
 import com.github.asciborek.settings.SettingsServiceProvider;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.Scopes;
-import com.google.inject.Singleton;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.slf4j.Logger;
@@ -17,24 +15,18 @@ public final class ApplicationModule extends AbstractModule {
   private static final Logger LOG = LoggerFactory.getLogger(ApplicationModule.class);
   private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-
   @Override
+  @SuppressWarnings("UnstableApiUsage")
   protected void configure() {
+    bind(EventBus.class).toInstance(new EventBus());
+    bind(ExecutorService.class).toProvider(this::executorService).in(Scopes.SINGLETON);
     bind(SettingsService.class).toProvider(SettingsServiceProvider.class).in(Scopes.SINGLETON);
   }
 
-  @Provides
-  @Singleton
-  public ExecutorService executorService() {
+  private ExecutorService executorService() {
     LOG.info("create executor service, available processors: {}", AVAILABLE_PROCESSORS);
     return Executors.newFixedThreadPool(AVAILABLE_PROCESSORS);
   }
 
-  @Provides
-  @Singleton
-  @SuppressWarnings("UnstableApiUsage")
-  public EventBus eventBus() {
-    return new EventBus();
-  }
 
 }
